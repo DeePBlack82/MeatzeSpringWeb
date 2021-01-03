c9a04c80ab89090901f9572983b8d597a837ac3d
DeePBlack82 <dapabai@gmail.com>
2021-01-03 20:33:39 +0100

    Actualización de formularios y funcionalidad

--- src/main/java/es/meatze/core/controller/Controlador.java
+++ src/main/java/es/meatze/core/controller/Controlador.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,60 +36,95 @@ public class Controlador {
 	
 	
 	@GetMapping("/")
-	public String inicio(Model modeloCentro, Model modeloAula) {
-		List<Centro> centros = centroService.listarCentros();
-		modeloCentro.addAttribute("centros", centros);
-		List<Aula> aulas = aulaService.listarAulas();
-		modeloAula.addAttribute("aulas", aulas);
+	public String inicio(Model modelo) {
+		
+		List<Centro> centros = centroService.listarCentros(); // Listado con todos los CENTROS
+		modelo.addAttribute("centros", centros);
+		
+		List<Aula> aulas = aulaService.listarAulas(); // Listado con todas las AULAS
+		modelo.addAttribute("aulas", aulas);
+		
 		return "index";
 	}
 	
 	@GetMapping("/buscar")
-	public String formularioBusqueda(Model modeloCentro, Model modeloBarakaldo, Model modeloBilbao, Model modeloNombrePeriferico, Model aulas, Model modeloRam, Model modeloAlmacenamiento) {
-		List<Centro> centros = centroService.listarCentros();
-		modeloCentro.addAttribute("centros", centros);
-		List<Aula> aulasBarakaldo = aulaService.aulasPorCentro("BA1%");
-		modeloBarakaldo.addAttribute("aulasBarakaldo", aulasBarakaldo);
-		List<Aula> aulasBilbao = aulaService.aulasPorCentro("BI1%");
-		modeloBilbao.addAttribute("aulasBilbao", aulasBilbao);
-		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos();
-		modeloNombrePeriferico.addAttribute("nombresPeri", nombresPerifericos);
-		List<String> aulasDeBaraka = aulaService.aulasDelCentro("BA1%");
-		aulas.addAttribute("aulas", aulasDeBaraka);
-		List<String> ram = ordenadorService.listarRAM();
-		modeloRam.addAttribute("ram", ram);
-		List<String> almacenamiento = ordenadorService.listarAlmacenamiento();
-		modeloAlmacenamiento.addAttribute("almacenamiento", almacenamiento);
+	public String formularioBusqueda(Model modelo) {
+		
+		/*
+		 * modelo.addAttribute("ordenadorABuscar", new Ordenador());
+		 * 
+		 */
+		
+		
+		
+		
+		List<Centro> centros = centroService.listarCentros(); // Listado con todos los CENTROS
+		modelo.addAttribute("centros", centros);
+		
+		List<Aula> aulasBarakaldo = aulaService.aulasPorCentro("BA1%"); // Listado de las AULAS de BARAKALDO
+		modelo.addAttribute("aulasBarakaldo", aulasBarakaldo);
+		
+		List<Aula> aulasBilbao = aulaService.aulasPorCentro("BI1%"); // Listado de las AULAS de BILBAO
+		modelo.addAttribute("aulasBilbao", aulasBilbao);
+		
+		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos(); // Listado de los NOMBRES de los PERIFÉRICOS
+		modelo.addAttribute("nombresPeri", nombresPerifericos);
+		
+		List<String> ram = ordenadorService.listarRAM(); // Listado de todas las RAM disponibles
+		modelo.addAttribute("ram", ram);
+		
+		List<String> almacenamiento = ordenadorService.listarAlmacenamiento(); // Listado de todos los DISCOS disponibles
+		modelo.addAttribute("almacenamiento", almacenamiento);
+		
 		return "formulario";
 	}
 	
 	@GetMapping("/listaPCs")
-	public String listaPCs(Model modeloCentro, Model allPCs, Model modeloNombrePeriferico, Model modeloRam, Model modeloAlmacenamiento) {
-		List<Centro> centros = centroService.listarCentros();
-		modeloCentro.addAttribute("centros", centros);
-		List<Ordenador> todosLosPCs = ordenadorService.listarOrdenadores();
-		allPCs.addAttribute("listaPCs", todosLosPCs);
-		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos();
-		modeloNombrePeriferico.addAttribute("nombresPeri", nombresPerifericos);
-		List<String> ram = ordenadorService.listarRAM();
-		modeloRam.addAttribute("ram", ram);
-		List<String> almacenamiento = ordenadorService.listarAlmacenamiento();
-		modeloAlmacenamiento.addAttribute("almacenamiento", almacenamiento);
+	public String listaPCs(Model modelo) {
+		
+		/*
+		 * modelo.addAttribute("ordenadorABuscar", new Ordenador());
+		 * 
+		 * 
+		 */
+		
+		
+		List<Centro> centros = centroService.listarCentros(); // Listado con todos los CENTROS
+		modelo.addAttribute("centros", centros);
+		
+		List<Ordenador> todosLosPCs = ordenadorService.listarOrdenadores(); // Listado con todos los ORDENADORES
+		modelo.addAttribute("listaPCs", todosLosPCs);
+		
+		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos(); // Listado de los NOMBRES de los PERIFÉRICOS
+		modelo.addAttribute("nombresPeri", nombresPerifericos);
+		
+		List<String> ram = ordenadorService.listarRAM(); // Listado de todas las RAM disponibles
+		modelo.addAttribute("ram", ram);
+		
+		List<String> almacenamiento = ordenadorService.listarAlmacenamiento(); // Listado de todos los DISCOS disponibles
+		modelo.addAttribute("almacenamiento", almacenamiento);
+		
 		return "lista-Pc";
 	}
 	
 	@GetMapping("/listaPerifericos")
-	public String listaPerifericos(Model modeloCentro, Model allPerifericos, Model modeloNombrePeriferico, Model modeloRam, Model modeloAlmacenamiento) {
-		List<Centro> centros = centroService.listarCentros();
-		modeloCentro.addAttribute("centros", centros);
-		List<Periferico> todosLosPerifericos = perifericoService.listarPerifericos();
-		allPerifericos.addAttribute("listaPerifericos", todosLosPerifericos);
-		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos();
-		modeloNombrePeriferico.addAttribute("nombresPeri", nombresPerifericos);
-		List<String> ram = ordenadorService.listarRAM();
-		modeloRam.addAttribute("ram", ram);
-		List<String> almacenamiento = ordenadorService.listarAlmacenamiento();
-		modeloAlmacenamiento.addAttribute("almacenamiento", almacenamiento);
+	public String listaPerifericos(Model modelo) {
+		
+		List<Centro> centros = centroService.listarCentros(); // Listado con todos los CENTROS
+		modelo.addAttribute("centros", centros);
+		
+		List<Periferico> todosLosPerifericos = perifericoService.listarPerifericos(); // Listado con todos los PERIFÉRICOS
+		modelo.addAttribute("listaPerifericos", todosLosPerifericos);
+		
+		List<String> nombresPerifericos = perifericoService.listarNombresPerifericos(); // Listado de los NOMBRES de los PERIFÉRICOS
+		modelo.addAttribute("nombresPeri", nombresPerifericos);
+		
+		List<String> ram = ordenadorService.listarRAM(); // Listado de todas las RAM disponibles
+		modelo.addAttribute("ram", ram);
+		
+		List<String> almacenamiento = ordenadorService.listarAlmacenamiento(); // Listado de todos los DISCOS disponibles
+		modelo.addAttribute("almacenamiento", almacenamiento);
+		
 		return "lista-Periferico";
 	}
 }
--- src/main/resources/static/js/formulario.js
+++ src/main/resources/static/js/formulario.js
@@ -1,6 +1,3 @@
-/**
- * 
- */
 var centro;
 var centroValue;
 let ordenador;
@@ -24,11 +21,16 @@ function inicio() {
 	ordenador.addEventListener("click", deseleccionPeri);
 	periferico.addEventListener("click", habilitarPeri);
 	periferico.addEventListener("click", deseleccionPc);
-	/*centro.addEventListener("change", borrarContenido);*/
-	/*centro.addEventListener("change", cambiaCentro);*/
+	/*centro.addEventListener("change", borrarContenido);
+	centro.addEventListener("change", cambiaCentro);*/
 	document.form.cbxMemoria.addEventListener("click", checkboxMemoria);
 	document.form.cbxAlmacenamiento.addEventListener("click", checkboxAlmacenamiento);
+	document.form.cbxPcAllInOne.addEventListener("click", checkboxAllInOne);
+	document.form.cbxPcOperativo.addEventListener("click", checkboxOperativoPC);
+	document.form.cbxPcUso.addEventListener("click", checkboxUsoPC);
 	document.form.cbxNombre.addEventListener("click", checkboxNombre);
+	document.form.cbxPeriOperativo.addEventListener("click", checkboxOperativoPeriferico);
+	document.form.cbxPeriUso.addEventListener("click", checkboxUsoPeriferico);
 	/*cargarMemoria();
 	cargarAlmacenamiento();
 	cargarPerifericos();*/
@@ -39,26 +41,35 @@ function deseleccionPc() {
 	document.form.cbxAlmacenamiento.checked=false;
 	document.form.cbxPcOperativo.checked=false;
 	document.form.cbxPcUso.checked=false;
-	document.form.selMemoria.value=0;
-	document.form.selAlmacenamiento.value=0;
+	document.form.cbxPcAllInOne.checked=false;
+	document.form.selMemoria.value="todas";
+	document.form.selAlmacenamiento.value="todos";
+	document.form.selAllInOne.value="todos";
+	document.form.selOperativoPC.value="todos";
+	document.form.selUsoPC.value="todos";
 }
 
 function deseleccionPeri() {
 	document.form.cbxNombre.checked=false;
 	document.form.cbxPeriOperativo.checked=false;
 	document.form.cbxPeriUso.checked=false;
+	document.form.selNombre.value="todos";
+	document.form.selOperativoPeriferico.value="todos";
+	document.form.selUsoPeriferico.value="todos";
 }
 
 function habilitarPc() {
 	document.form.cbxMemoria.disabled=false;
 	document.form.cbxAlmacenamiento.disabled=false;
+	document.form.cbxPcAllInOne.disabled=false;
 	document.form.cbxPcOperativo.disabled=false;
 	document.form.cbxPcUso.disabled=false;
 	document.form.cbxNombre.disabled=true;
 	document.form.selNombre.disabled=true;
-	document.form.selNombre.value=0;
 	document.form.cbxPeriOperativo.disabled=true;
+	document.form.selOperativoPeriferico.disabled=true;
 	document.form.cbxPeriUso.disabled=true;
+	document.form.selUsoPeriferico.disabled=true;
 }
 
 function habilitarPeri() {
@@ -66,31 +77,35 @@ function habilitarPeri() {
 	document.form.selMemoria.disabled=true;
 	document.form.cbxAlmacenamiento.disabled=true;
 	document.form.selAlmacenamiento.disabled=true;
+	document.form.cbxPcAllInOne.disabled=true;
+	document.form.selAllInOne.disabled=true;
 	document.form.cbxPcOperativo.disabled=true;
+	document.form.selOperativoPC.disabled=true;
 	document.form.cbxPcUso.disabled=true;
+	document.form.selUsoPC.disabled=true;
 	document.form.cbxNombre.disabled=false;
 	document.form.cbxPeriOperativo.disabled=false;
 	document.form.cbxPeriUso.disabled=false;
 }
 
-function cambiaCentro() {
+/*function cambiaCentro() {
 	let centro = document.getElementById("centro");
 	let centroSeleccionado = centro.value;
-	/*alert([["${aulasBarakaldo}"]]);*/
+	alert([["${aulasBarakaldo}"]]);
 	
 	
 		if (centroSeleccionado == "BA1") {
 			let aulas = document.getElementById("aula");
 			for (var i=0; i<centroBarakaldo.length; i++) {
 				let opcion = document.createElement("option");
-				opcion.text = /*${aulasBarakaldo.nombre};*/centroBarakaldo[i];
+				opcion.text = ${aulasBarakaldo.nombre};centroBarakaldo[i];
 				aulas.appendChild(opcion);
 			}
-			/*for (var i=0; i<"${#lists.size(aulasBarakaldo)}"; i++) {
+			for (var i=0; i<"${#lists.size(aulasBarakaldo)}"; i++) {
 				let opcion = document.createElement("option");
 				opcion.text = "${aulasBarakaldo.nombre}";centroBarakaldo[i];
 				aulas.appendChild(opcion);
-			}*/
+			}
 		}
 		if (centroSeleccionado == "BI1") {
 			let aulas = document.getElementById("aula");
@@ -130,48 +145,19 @@ function borrarAulasInsertarAulas() {
 		let aulasBarakaldo = '<option th:each="aulasBa:${aulasBarakaldo}" th:text="${aulasBa.id_aula}" th:id="${aulasBa.id_aula}" th:name="${aulasBa.id_aula}" th:value="${aulasBa.id_aula}"></option>';
 		aulas.innerHTML += aulasBarakaldo;
 		document.write('<p th:each="aulasBa:${aulasBarakaldo}" th:text="${aulasBa.id_aula}" th:id="${aulasBa.id_aula}" th:name="${aulasBa.id_aula}" th:value="${aulasBa.id_aula}">Hola</p>');
-		/*for (var i=0; i<centroBarakaldo.length; i++) {
+		for (var i=0; i<centroBarakaldo.length; i++) {
 			let opcion = document.createElement("option");
 			opcion.text = ${aulasBarakaldo.nombre};centroBarakaldo[i];
 			aulas.appendChild(opcion);
-		}*/
+		}
 	}
 	else if (centroSeleccionado == "BI1") {
 		let aulasBilbao = '<option th:each="aulasBi:${aulasBilbao}" th:text="${aulasBi.id_aula}" th:id="${aulasBi.id_aula}" th:name="${aulasBi.id_aula}" th:value="${aulasBi.id_aula}"></option>';
 		aulas.innerHTML += aulasBilbao;
 	}
 
-}
-
-/*function cargarMemoria() {
-	let selMemoria = document.getElementById("selMemoria");
-	let arrayMemoria = ["2 GB", "3 GB", "4 GB", "5 GB", "6 GB", "8 GB"];
-	for (var i=0; i<arrayMemoria.length; i++) {
-		let opcion = document.createElement("option");
-		opcion.text = arrayMemoria[i];
-		selMemoria.appendChild(opcion);
-	}
-}
-
-function cargarAlmacenamiento() {
-	let selAlmacenamiento = document.getElementById("selAlmacenamiento");
-	let arrayAlmacenamiento = ["100 GB", "250 GB", "500 GB", "1 TB"];
-	for (var i=0; i<arrayAlmacenamiento.length; i++) {
-		let opcion = document.createElement("option");
-		opcion.innerText = arrayAlmacenamiento[i];
-		selAlmacenamiento.appendChild(opcion);
-	}
-}
-
-function cargarPerifericos() {
-	let selNombre = document.getElementById("selNombre");
-	let arrayNombre = ["Altavoces", "Cámara", "Monitor", "Proyector", "Ratón", "Teclado"];
-	for (var i=0; i<arrayNombre.length; i++) {
-		let opcion = document.createElement("option");
-		opcion.innerText = arrayNombre[i];
-		selNombre.appendChild(opcion);
-	}
 }*/
+
 
 function checkboxMemoria() {
 	if (document.form.cbxMemoria.checked) {
@@ -179,7 +165,7 @@ function checkboxMemoria() {
 	}
 	else {
 		document.form.selMemoria.disabled=true;
-		document.form.selMemoria.value=0;
+		document.form.selMemoria.value="todas";
 	}
 }
 
@@ -189,7 +175,37 @@ function checkboxAlmacenamiento() {
 	}
 	else {
 		document.form.selAlmacenamiento.disabled=true;
-		document.form.selAlmacenamiento.value=0;
+		document.form.selAlmacenamiento.value="todos";
+	}
+}
+
+function checkboxAllInOne() {
+	if (document.form.cbxPcAllInOne.checked) {
+		document.form.selAllInOne.disabled=false;
+	}
+	else {
+		document.form.selAllInOne.disabled=true;
+		document.form.selAllInOne.value="todos";
+	}
+}
+
+function checkboxOperativoPC() {
+	if (document.form.cbxPcOperativo.checked) {
+		document.form.selOperativoPC.disabled=false;
+	}
+	else {
+		document.form.selOperativoPC.disabled=true;
+		document.form.selOperativoPC.value="todos";
+	}
+}
+
+function checkboxUsoPC() {
+	if (document.form.cbxPcUso.checked) {
+		document.form.selUsoPC.disabled=false;
+	}
+	else {
+		document.form.selUsoPC.disabled=true;
+		document.form.selUsoPC.value="todos";
 	}
 }
 
@@ -199,7 +215,26 @@ function checkboxNombre() {
 	}
 	else {
 		document.form.selNombre.disabled=true;
-		document.form.selNombre.value=0;
+		document.form.selNombre.value="todos";
 	}
 }
 
+function checkboxOperativoPeriferico() {
+	if (document.form.cbxPeriOperativo.checked) {
+		document.form.selOperativoPeriferico.disabled=false;
+	}
+	else {
+		document.form.selOperativoPeriferico.disabled=true;
+		document.form.selOperativoPeriferico.value="todos";
+	}
+}
+
+function checkboxUsoPeriferico() {
+	if (document.form.cbxPeriUso.checked) {
+		document.form.selUsoPeriferico.disabled=false;
+	}
+	else {
+		document.form.selUsoPeriferico.disabled=true;
+		document.form.selUsoPeriferico.value="todos";
+	}
+}
